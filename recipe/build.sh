92ea1948d0e072e64b189ac5bf6275e310d7206f
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-01-15 14:54:40 -0600

    patch in PREFIX/share/hunspell, remove post-link.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,24 @@
+#!/usr/bin/env bash
+set -eux
+
+sed -i \
+    "s;^#define LIBDIR ;#define LIBDIR \"${PREFIX}/share/hunspell:${PREFIX}/share/hunspell_dictionaries:\" ;" \
+    tools/hunspell.cxx
+
 autoreconf -vfi
-./configure --prefix=$PREFIX --with-readline --with-ui
+
+./configure \
+    --prefix="${PREFIX}" \
+    --with-readline \
+    --with-ui
+
 make
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-make check
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]
+then
+    make check
 fi
+
 make install
-mv $PREFIX/bin/hunspell $PREFIX/bin/.hunspell
 
-# We have to make a wrapper to add a spelling dictionary path to hunspell,
-# since none of the default ones are relative to the binary (i.e., can be
-# installed as a conda package)
-cat <<EOF > $PREFIX/bin/hunspell
-#!/bin/sh
-export DICPATH='$PREFIX/share/hunspell_dictionaries'
-$PREFIX/bin/.hunspell "\$@"
-EOF
-
-chmod a+x $PREFIX/bin/hunspell
+chmod a+x "${PREFIX}/bin/hunspell"
