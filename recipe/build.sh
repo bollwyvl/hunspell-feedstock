d24515ab97b25acf1d280cc6a0676d2f1a5a5afd
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-01-15 19:00:51 -0600

    linting

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-set -eux
+set -euxo pipefail
 
 # Add hunspell dictionary paths relative to PREFIX. We have to do this here so
 # that $PREFIX gets expanded into the patch.
 sed "s;\$CONDA_PREFIX;${PREFIX};"\
-    < "$RECIPE_DIR/0000-dictionary-search-path.patch" \
+    < "${RECIPE_DIR}/0000-dictionary-search-path.patch" \
     | patch -p1
 
 autoreconf -vfi
