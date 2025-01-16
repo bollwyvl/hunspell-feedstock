d01ab21e12b346be2671540e6fadcb5dcefb8780
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-01-15 18:57:50 -0600

    split patch out of heredoc

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,21 +3,9 @@ set -eux
 
 # Add hunspell dictionary paths relative to PREFIX. We have to do this here so
 # that $PREFIX gets expanded into the patch.
-patch -p1 <<EOF
-diff --git a/src/tools/hunspell.cxx b/src/tools/hunspell.cxx
---- a/src/tools/hunspell.cxx
-+++ b/src/tools/hunspell.cxx
-@@ -116,6 +116,8 @@
- #include "../parsers/odfparser.hxx"
-
- #define LIBDIR                \
-+  "${PREFIX}/share/hunspell:" \
-+  "${PREFIX}/share/hunspell_dictionaries:" \
-   "/usr/share/hunspell:"      \
-   "/usr/share/myspell:"       \
-   "/usr/share/myspell/dicts:" \
-
-EOF
+sed "s;\$CONDA_PREFIX;${PREFIX};"\
+    < "$RECIPE_DIR/0000-dictionary-search-path.patch" \
+    | patch -p1
 
 autoreconf -vfi
 
